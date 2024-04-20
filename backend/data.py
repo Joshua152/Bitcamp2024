0b681067084d9f8569afa2a664a2789b5df122b5
Mark Fleckser <markfleckser@wireless-10-105-222-2.umd.edu>
2024-04-20 13:40:29 -0400

    fixed api key

--- backend/data.py
+++ backend/data.py
@@ -9,8 +9,8 @@ price: (min, max) touple with acceptable range of prices
 returns: list of house dicts with keys: attomId, lotsize, address, bathstotal, beds, roomsTotal, listingPrice
 '''
 def get_houses(zipcode, beds, baths, price):
-    url = f"https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/detail?postalCode={zipcode}&minBeds={beds[0]}&maxBeds={beds[1]}&minBathsTotal={baths[0]}&maxBathsTotal={baths[1]}&minAVMValue={price[0]}&maxAVMValue={price[1]}&debug=True"
-    apiKey = "2b1e86b638620bf2404521e6e9e1b19e"
+    url = f"https://api.gateway.attomdata.com/propertyapi/v1.0.0/attomavm/detail?postalCode={zipcode}&minBeds={beds[0]}&maxBeds={beds[1]}&minBathsTotal={baths[0]}&maxBathsTotal={baths[1]}&minAVMValue={price[0]}&maxAVMValue={price[1]}&debug=True"
+    apiKey = "298da253ad4e02f33880f66bb5089de2"
 
     headers = {
         "Accept": "application/json",
@@ -29,18 +29,10 @@ def get_houses(zipcode, beds, baths, price):
             "address": house["address"]["oneLine"],
             "bathstotal": house["building"]["rooms"]["bathstotal"],
             "beds": house["building"]["rooms"]["beds"],
-            "roomsTotal": house["building"]["rooms"]["roomsTotal"]
+            "roomsTotal": house["building"]["rooms"]["roomsTotal"],
+            "listingPrice": house["avm"]["amount"]["value"]
         }
-
-        attomId = data["attomId"]
-
-        avm_url = f"https://api.gateway.attomdata.com/propertyapi/v1.0.0/attomavm/detail?attomid={attomId}"
-        avm_res = requests.get(avm_url, headers=headers)
-
-        properties = avm_res.json()["property"]
-        if len(properties) != 0:
-            data["listingPrice"] = properties[0]["avm"]["amount"]["value"]
             
-            houses.append(data)
+        houses.append(data)
 
     return houses
